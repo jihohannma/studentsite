bf5519d0a6e9bf0bc41a2a6cce5196ce54d00aa8
jihohannma <hanjiho@hawaii.edu>
2021-03-20 19:56:59 -1000

    Base site about subpages

---
+++ art128/base-site-subpages/js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // TOGGLE NAV MOBILE MANU FOR SMALL SCREENS 
+    const menubutton =  document.querySelector('.menu-button');
+    const menunav =  document.querySelector('.toggle-nav');
+    menubutton.addEventListener('click', function() {
+        if (menunav.getAttribute('data-navstate') === 'open') {
+            menunav.setAttribute('data-navstate', 'closed');
+        } else {
+            menunav.setAttribute('data-navstate', 'open');
+        };
+    });
+
+});
